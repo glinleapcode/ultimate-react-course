a8771cdd5fd930291168c33deb4efc2c5cf6be81
Brenda <ziruhe@gmail.com>
2024-01-01 20:18:44 +0000

    Jan 1, 2024, 12:18 PM

--- 05-travel-list/1-1-24/faraway-travel-app/src/App.js
+++ 05-travel-list/1-1-24/faraway-travel-app/src/App.js
@@ -1,5 +1,10 @@
 import "./index.css";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
   return (
     <div className="App">
@@ -24,7 +29,21 @@ function Form() {
 }
 
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <div className="list">
+      <li>{item.description}</li>
+    </div>
+  );
 }
 
 function Stats() {
