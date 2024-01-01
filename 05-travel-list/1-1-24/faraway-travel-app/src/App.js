b76faae8d30296c7f6a317d9db7537fd9dc7b7e7
Brenda <ziruhe@gmail.com>
2024-01-01 20:42:07 +0000

    Jan 1, 2024, 12:42 PM

--- 05-travel-list/1-1-24/faraway-travel-app/src/App.js
+++ 05-travel-list/1-1-24/faraway-travel-app/src/App.js
@@ -23,9 +23,16 @@ function Logo() {
 
 function Form() {
   return (
-    <div className="add-form">
+    <form className="add-form">
       <h3>What do you need for your trip? 😍</h3>
-    </div>
+      <select name="" id="">
+        <option value={1}>1</option>
+        <option value={2}>2</option>
+        <option value={3}>3</option>
+      </select>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
   );
 }
 
