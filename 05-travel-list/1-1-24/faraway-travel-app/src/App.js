300a12a05c3de42b99443910ed0c1bb3d6c7d8fc
Brenda <ziruhe@gmail.com>
2024-01-01 20:48:27 +0000

    Jan 1, 2024, 12:48 PM

--- 05-travel-list/1-1-24/faraway-travel-app/src/App.js
+++ 05-travel-list/1-1-24/faraway-travel-app/src/App.js
@@ -26,7 +26,11 @@ function Form() {
     <form className="add-form">
       <h3>What do you need for your trip? 😍</h3>
       <select name="" id="">
-        {Array.from({ length: 20 }, (_, i) => i + 1)}
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
