1b65403936a7c8386691fd8eac215792bef0bad3
Brenda <ziruhe@gmail.com>
2024-01-01 20:04:29 +0000

    Jan 1, 2024, 12:04 PM

--- 05-travel-list/1-1-24/faraway-travel-app/src/App.js
+++ 05-travel-list/1-1-24/faraway-travel-app/src/App.js
@@ -4,4 +4,14 @@ function App() {
   return <div className="App"></div>;
 }
 
+function Logo() {
+  return;
+}
+
+function Form() {}
+
+function PackingList() {}
+
+function Stats() {}
+
 export default App;
