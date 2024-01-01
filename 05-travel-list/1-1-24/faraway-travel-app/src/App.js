e0e7a4164e06ef9d233e48945a31758dad8e6d8a
Brenda <ziruhe@gmail.com>
2024-01-01 20:43:30 +0000

    Jan 1, 2024, 12:43 PM

--- 05-travel-list/1-1-24/faraway-travel-app/src/App.js
+++ 05-travel-list/1-1-24/faraway-travel-app/src/App.js
@@ -26,9 +26,7 @@ function Form() {
     <form className="add-form">
       <h3>What do you need for your trip? 😍</h3>
       <select name="" id="">
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
+        {}
       </select>
       <input type="text" placeholder="Item..." />
       <button>Add</button>
