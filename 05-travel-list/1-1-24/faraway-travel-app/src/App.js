e0cf4f84f095b21e362c6f0b8af2bbd0e73279d7
Brenda <ziruhe@gmail.com>
2024-01-01 22:34:35 +0000

    Jan 1, 2024, 2:34 PM

--- 05-travel-list/1-1-24/faraway-travel-app/src/App.js
+++ 05-travel-list/1-1-24/faraway-travel-app/src/App.js
@@ -24,7 +24,7 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState("0");
+  const [quantity, setQuantity] = useState("1");
   function handleSubmit(e) {
     e.preventDefault(); // prevent reload when submit
     console.log(e);
@@ -33,7 +33,12 @@ function Form() {
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip? 😍</h3>
-      <select name="" id="">
+      <select
+        name=""
+        id=""
+        value={quantity}
+        onChange={(e) => setQuantity(e.target.value)}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
